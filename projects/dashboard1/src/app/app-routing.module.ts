dd8e01bc223ba84a3426b4d2d32c30f1e367df51
minhmo3004 <minhmo016366@gmail.com>
2025-09-11 17:44:17 +0700

    fix scss

--- projects/dashboard1/src/app/app-routing.module.ts
+++ projects/dashboard1/src/app/app-routing.module.ts
@@ -10,10 +10,7 @@ const routes: Routes = [
     path: 'dashboard',
     loadComponent: () => import('./modules/dashboard/components/main-component/main-component.component').then(m => m.MainComponentComponent)
   },
-  {
-    path: 'detail/:id/:name',
-    loadComponent: () => import('./modules/meter-manager/components/details/details.component').then(m => m.DetailsComponent)
-  },
+ 
   {
     path: 'water-clock',
     loadComponent: () => import('./modules/water-clock/components/water-meter-info/water-meter-info.component').then(m => m.WaterMeterInfoComponent)
